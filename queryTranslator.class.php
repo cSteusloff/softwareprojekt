eda789cfef523278fb128680913bc029f9bf5abe
Jens Wiemann <jens.wiemann@hotmail.com>
2013-12-13 21:30:34 +0100

    queryTranslator errors fixed

--- queryTranslator.class.php
+++ queryTranslator.class.php
@@ -8,6 +8,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <?php 
 require_once 'php-sql-parser.php';
+//https://code.google.com/p/php-sql-parser/
 
 echo("<pre>");
 
@@ -15,54 +16,91 @@ $qT = new queryTranslator();
 //Create X/ Delete V/ Update V/ Insert Into X
 //print_r($qT->translate("INSERT INTO kunden (name, adresse) VALUES ('Hans Meier', 'Wuppernweg 19')","JENS_"));
 //print_r($qT->translate("Create Table: CREATE TABLE t (  id INT(11) default NULL auto_increment,  s char(60) default NULL,  PRIMARY KEY (id))","JENS_"));
-print_r($qT->translate("UPDATE kunden SET name = 'Donald Duck', adresse = 'Entenhausen' WHERE name = 'Emil Entenich'","JENS_"));
+print_r($qT->translate("CREATE TABLE example (
+         id INT;
+         data VARCHAR(100)
+     );", "Jens_"));
+//print_r($qT->translate("UPDATE kunden SET name = 'Donald Duck', adresse = 'Entenhausen' WHERE name = 'Emil Entenich' Order by 'Haus'","JENS_"));
+/*print_r($qT->translate("
+SELECT 
+  MASTER_Cocktail.cname as Cocktail, 
+  z.zname as Zutat, 
+  zc.menge 
+FROM 
+  MASTER_Cocktail, 
+  MASTER_Zutat z, 
+  MASTER_Zutat_Cocktail zc 
+WHERE 
+  c.cid = zc.cid AND zc.zid = z.zid AND alkoholisch LIKE 'y'", "JENS_"));
+*/
+//print_r($qT->translate("SELECT Affe FROM Djungel ORDER BY Banane","JENS_"));
+
 
 
 class queryTranslator {
 	
 	//Add $username in front of every tablename of $inputquery
     public function translate($inputquery, $username){
-        $parser = new PHPSQLParser();
-		$parsed = $parser->parse($inputquery, TRUE);
-		//print_r($parsed);
-		$positions = $this->transformarray($parsed);
+        try{
+			$parser = new PHPSQLParser();
+			$parsed = $parser->parse($inputquery, TRUE);
+			//print_r($parsed);
+			$positions = $this->nameSearch($parsed);
 		
-		$i=0;
-		foreach($positions as $pos){
-			$inputquery = substr_replace($inputquery, $username, $pos+strlen($username)*$i, 0);
-			++$i;
+			$i=0;
+			foreach($positions as $pos){
+				$inputquery = substr_replace($inputquery, $username, $pos+strlen($username)*$i, 0);
+				++$i;
+			}
 		}
-		//print_r($inputquery);
+		catch( Exception $e)
+		{
+			return $e;
+		}
 		return $inputquery;			
     }
+	//Search for Names in Query
+	private function nameSearch($parsedarray){	
+		function search2($array, $key) { 
+			$results = array(); 
+
+			if (is_array($array)){ 
+				if (isset($array[$key])) 
+					$results[] = $array; 
 	
-	//Returns the positions of the Tablenames of the query
-	public function transformarray($parsedarray){	
-		
-		function printValue($value, $key, $userData) 
-		{
-			if($key == "table" || $key == 'position'){
-				$userData[] = $value;
-			} 
-		}
-		//Lists all table names and positions (in the query)
-		$tempresult = new ArrayObject();
-		array_walk_recursive($parsedarray, 'printValue', $tempresult);
-		//print_r($tempresult);
+				foreach ($array as $subarray) 
+					$results = array_merge($results, search2($subarray, $key)); 
+			}
+			return $results; 
+		} 		
+		function search($array, $key, $value) { 
+			$results = array(); 
+
+			if (is_array($array)){ 
+				if (isset($array[$key]) && $array[$key] == $value) 
+					$results[] = $array; 
+	
+				foreach ($array as $subarray) 
+					$results = array_merge($results, search($subarray, $key, $value)); 
+			}
+			return $results; 
+		} 
+		//Change if other type required
+		//$tempresult = search($parsedarray, "expr_type", "table");
+		$tempresult = search2($parsedarray,"table");
+		$answer = new ArrayObject();
 		
 		//Returns ownly the positions matching to the tables
 		foreach($tempresult as $key => $val){
-			if(!is_numeric($tempresult[$key]) && !is_null($tempresult[$key+1]) && is_numeric($tempresult[$key+1]))
-				$answer[] = $tempresult[$key+1]; // next element
+			foreach($tempresult[$key] as $key2 => $val2){
+				if ($key2 == "position")	
+					$answer[] = $val2;
+				}
 		}
-		print_r($answer);
-		
 		return $answer;
-    }
-	
+	}
 	//TODO: check for forbidden commands
     private function checkForbidden(){
 
     }
-
 } 
